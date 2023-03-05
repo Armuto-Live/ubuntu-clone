55712bbdd88a2f37f448b386b113579c95f2b90f
Armuto-Live <luisarmuto3@gmail.com>
2023-03-05 10:18:57 -0500

    add data footer

---
+++ src/constants/Footer.js
@@ -0,0 +1,37 @@
+const DataFooter=[
+    {
+        id:0,
+        title:"OpenStack",
+        datails:["What is OpenStack","Features","Managed","Consulting","Install","Support"]
+    },
+    {
+        id:1,
+        title:"Ceph",
+        datails:["What is Ceph","Managed","Consulting","Docs","Install"]
+    },
+    {
+        id:2,
+        title:"Managed",
+        datails:["OpenStack","Kubernetes","Ceph","Apps","Observability"]
+    },
+    {
+        id:3,
+        title:"Kubernetes",
+        datails:["What is Kubernetes","Charmed Kubernetes","Managed","Install","Docs","Resources"]
+    },
+    {
+        id:4,
+        title:"AI/ML",
+        datails:["What is Kubeflow","Services"]
+    },
+    {
+        id:5,
+        title:"Robotics",
+        datails:["ROS ESM","What is ROS","Community"]
+    },
+    {
+        id:6,
+        title:"IOT",
+        datails:["App store","Embedded Linux","EdgeX","Management"]
+    },
+]
